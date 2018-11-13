92d45c8db7fe4f3bd4b4fa18f4b1de5771275d1e
hanqiang <hanqiang@yeteam.com>
2018-11-13 18:46:13 +0800

    component state

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,11 +1,22 @@
 import * as React from 'react';
 
+/**
+ * 组件外部状态
+ */
 export interface HelloProps {
     compiler: string;
     framework: string;
 }
 
-export class Hello extends React.Component<HelloProps, {}> {
+/**
+ * 组件内部状态
+ */
+export interface State {
+
+}
+
+export class Hello extends React.Component<HelloProps, State> {
+
     render() {
         return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>
     }
